40ce1897997a8669872a0b46a8b12622eccfd535
akalmannakarmi <akalnakarmi1@gmail.com>
2025-10-22 14:24:32 +0545

    added a service to create a user

---
+++ app/services/user_service.py
@@ -0,0 +1,12 @@
+from app.db.models.user import User
+from app.db.session import SessionLocal
+from app.core.security import get_password_hash
+import asyncio
+
+
+def create_user(email: str, password: str):
+    db = SessionLocal()
+    hashed_pw = get_password_hash(password)
+    user = User(email=email, hashed_password=hashed_pw)
+    db.add(user)
+    asyncio.run(db.commit())
