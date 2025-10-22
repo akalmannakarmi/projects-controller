34012b46a57ab4859e5f29d4453280890861bdd5
akalmannakarmi <akalnakarmi1@gmail.com>
2025-10-22 14:24:02 +0545

    added app schemas

---
+++ app/schemas/user.py
@@ -0,0 +1,28 @@
+from pydantic import BaseModel, EmailStr
+
+
+class UserBase(BaseModel):
+    email: EmailStr
+    is_active: bool = True
+
+
+class UserCreate(UserBase):
+    password: str
+
+
+class UserRead(UserBase):
+    id: int
+
+    model_config = {
+        "from_attributes": True,
+    }
+
+
+class UserLogin(BaseModel):
+    email: EmailStr
+    password: str
+
+
+class Token(BaseModel):
+    access_token: str
+    refresh_token: str
